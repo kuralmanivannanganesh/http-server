3f89cc20550b1946ffd29502637c0ff3e3f41e5a
Kural Manivannan <kuralmanivannanganesh@gmail.com>
2026-01-20 15:07:15 +0530

    added user agent

--- app/main.py
+++ app/main.py
@@ -18,6 +18,15 @@ def send_res_message(conn, msg):
     res = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {data_len}\r\n\r\n{msg}"
     conn.sendall(res.encode())
 
+def parse_headers(data):
+    headers = {}
+    header_lines = data.split("\r\n")
+    for line in header_lines:
+        if ":" in line:
+            k, v = line.split(":", 1)
+            headers[k.strip().lower()] = v.strip()
+    return headers
+
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -41,14 +50,17 @@ def main():
         print()
         data = recource.split("/")[2]
         send_res_message(conn=conn, msg=data)
-        # data_len = len(data)
-        # res = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {data_len}\r\n\r\n{data}"
-        # conn.sendall(res.encode())
     elif recource.startswith("/user-agent"):
-        for header in message_split:
-            if header.startswith("User-Agent"):
-                agent = header.split(":")[1].strip()
-                send_res_message(conn=conn, msg=agent)
+        headers = parse_headers(message)
+        print(f"[HEADERS] : {headers}")
+        if "user-agent" in headers:
+            print("Sending user agent")
+            send_res_message(conn=conn, msg=headers.get("user-agent"))
+
+        # for header in message_split:
+        #     if header.startswith("User-Agent"):
+        #         agent = header.split(":")[1].strip()
+        #         send_res_message(conn=conn, msg=agent)
     else:
         conn.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
     conn.close()
